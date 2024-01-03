ae42b7a37abbebe55959b6de1cb5b498f28df6eb
Nassim Sakhri <tails01@hotmail.fr>
2024-01-03 17:13:20 +0100

    Finalisation de l'IHM

--- Exo_Todo/src/main/java/Main.java
+++ Exo_Todo/src/main/java/Main.java
@@ -36,76 +36,7 @@ public class Main {
                 default -> {
                     System.out.println("Réponse invalide");
                 }
-
             }
-
         } while (choice != 6);
-
     }
 }
-
-
-//    Scanner  scan = new Scanner(System.in);
-//    ToDoDAOImpl  toDoDAO = new ToDoDAOImpl();
-//    int tache_num;
-//    int tache_mod;
-//
-//
-//        System.out.println("ecrivez une tache");
-//    String task = scan.nextLine();
-//    ToDo tache = new ToDo(task, false);
-//        toDoDAO.Ajout(tache);
-//
-////         Voir toutes les taches :
-//
-//        toDoDAO.Obtention();
-//
-//
-//    // Voir une tache :
-//
-//        toDoDAO.Obtention();
-//        System.out.println("Choisissez quelle tâche modifier");
-//    tache_num = scan.nextInt();
-//        scan.nextLine();
-//        toDoDAO.ObtentionUnique(tache_num);
-//
-//    //         Modifier  une tache :
-//
-//    menutache();
-//    tache_mod = scan.nextInt();
-//        scan.nextLine();
-//
-//        while (tache_mod != 1 && tache_mod != 2) {
-//        System.out.println("invalide.");
-//        menutache();
-//        tache_mod = scan.nextInt();
-//        scan.nextLine();
-//    }
-//    ;
-//
-//        if (tache_mod == 1) {
-//        System.out.println("Merci d'écrire un nouveau nom de tâche.");
-//        String nouveauNom = scan.nextLine();
-//        toDoDAO.MiseAJourNom(tache_num, nouveauNom);
-//    } else {
-//        toDoDAO.MiseAJourEtat(tache_num);
-//    }
-//
-//    // Supprimer une tache :
-//
-//            toDoDAO.Obtention();
-//            System.out.println("Choisissez quelle tâche supprimer");
-//    tache_num = scan.nextInt();
-//            scan.nextLine();
-//            toDoDAO.Suppression(tache_num);
-//
-//
-//        scan.close();
-//
-//}
-//
-//    public static void menutache() {
-//        System.out.println("Que souhaitez vous modifier ?");
-//        System.out.println("1 - Modifier le nom de la tâche.");
-//        System.out.println("2 - Modifier l'état de la tâche.");
-//    }
